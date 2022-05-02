6aa5e7b3512fe309d03540677d51fa5e51a3fd18
Sage <sagewhite24@gmail.com>
2022-05-01 23:47:59 -0400

    pages

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Contactmain from "../components/Contactmain";
+import Office from "../components/Office";
 
 function Contact() {
   return (
     <div>
       <Nav />
+      <Contactmain />
+      <Office />
     </div>
   );
 }
---
+++ src/components/Contactmain.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contactmain() {
+  return (
+    <section class="">
+      <h1 className="contact-mess">
+        We Would Be Delighted To<br></br> Hear From You.
+      </h1>
+    </section>
+  );
+}
+
+export default Contactmain;
